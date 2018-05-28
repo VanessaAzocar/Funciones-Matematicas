e2edacf76fafff3b3d64423c8c01779d275b516e
Vanessa Azócar <v.azocar.adasme@gmail.com>
2018-05-28 17:57:38 -0300

    funcion con console.log

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ La función add debe sumar sus argumentos y devolver el resultado.
 La funcion multiply debe multiplicar sus argumentos.
 Usando solamente estas dos funciones, resuelva este simple problema matemático:
 36325 * (9824 + 777).*/
+const add= function(arg1, arg2){
+    return arg1+arg2;
+}
+const multiply= function(arg3, arg4){
+    return arg3*arg4;
+}
+const one = add(9824, 777);
+
+const final = multiply(36325, one);
+
+console.log(final); 
